515d97faf5df42bf49d4c019a92a74cc0afa3d9f
wildcatandapple <saghwa3@gmailc.om>
2023-01-17 14:34:32 +0900

    Initial Commit

---
+++ playgrond3/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  playgrond3
+//
+//  Created by isaac hwang on 2023/01/17.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
